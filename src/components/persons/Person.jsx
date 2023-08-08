acdbca5fd3e7982c83713c2987653be9867ab981
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-08 11:32:38 +0300

    uzd

--- src/components/persons/Person.jsx
+++ src/components/persons/Person.jsx
@@ -1,4 +1,10 @@
 // Person
 // <Person name='Bugs Bunny' age={25} gender={"male"} hobby={"reading"}>
-
+// lyties dalis neturetu sakyti 'male' arba 'female' o turetu sakyti 'Vyras' arba "Moteris"
 // atvaizduoti vyrus ir moteris skirtinga fono spalva (className)
+
+// prideti mygtuka "Make special"
+// 1. mygtukas turetu uzdeti klase kuri uzdeda 5px geltona remeli
+// 2. paspaudus mygtuka antra karta klase nuimama ir remelis pasidaro nebespecial
+// 3. pakeisti teksta "Make special" i "Make normal" priklausomai pagal tai ar kortele
+// special ar ne.
--- src/components/persons/PersonList.jsx
+++ src/components/persons/PersonList.jsx
@@ -1,7 +1,8 @@
+// PERSONLIST
 // importuosim is data.js duomenis
 
 // suksim cikla ir generuosim Person componentus
 
-// (sunkesnis veliau)atrinkti tik vyriskos lyties persons
-
 // exportuoti ir importuot i App.jsx
+
+// (sunkesnis veliau)atrinkti tik vyriskos lyties persons
