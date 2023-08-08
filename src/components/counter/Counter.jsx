363c941ee2543d7203c0b5bcaf7825547b0023ec
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-08 09:53:03 +0300

    simple variable not working

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,12 +1,22 @@
 import Grid from '../UI/Grid';
 
 function Counter() {
+  console.log('<Counter generated />');
+
+  let countNum = 25;
+
+  function goUp() {
+    console.count('goUp');
+    countNum++;
+    console.log('countNum ===', countNum);
+  }
+
   return (
     <div className='counter'>
       <h2>Label</h2>
-      <p className='counterValue'>0</p>
+      <p className='counterValue'>{countNum}</p>
       <Grid cols='2'>
-        <button>UP</button>
+        <button onClick={goUp}>UP</button>
         <button>Down</button>
       </Grid>
     </div>
