1c00fd33dfce1e74239b934023e8e23866650540
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-08 09:16:41 +0300

    text

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,12 @@
 function Card(props) {
+  // console.log('props ===', props);
+  // const heading = props.heading;
+  // const text = props.text;
+  const { heading, text } = props;
   return (
     <div className='card'>
-      <h3 className='cardTitle'>{props.heading}</h3>
-      <p className='cardText'>text</p>
+      <h3 className='cardTitle'>{heading}</h3>
+      <p className='cardText'>{text}</p>
     </div>
   );
 }
