9b6c40bb20bbb8d1f5f9859f36cb8f42f2d5aee2
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-08 08:48:49 +0300

    card html cardlist html + css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './styles/index.css';
 import './styles/App.css';
+import CardList from './components/CardList';
 
 function App() {
   return (
     <div className='app'>
       <h1>Hello Vite</h1>
+      <CardList />
     </div>
   );
 }
