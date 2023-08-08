3cf89c5519f8639db04c5d9bdf9a92c07afbbdef
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-08 08:39:39 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './styles/index.css';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Hello Vite</h1>
+    </div>
+  );
+}
+export default App;
