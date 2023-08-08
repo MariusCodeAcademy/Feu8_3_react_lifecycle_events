fd80737ec0c76ecdc95cc9197f9ee88532d22284
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-08 11:21:34 +0300

    uzd

---
+++ src/components/persons/Person.jsx
@@ -0,0 +1,4 @@
+// Person
+// <Person name='Bugs Bunny' age={25} gender={"male"} hobby={"reading"}>
+
+// atvaizduoti vyrus ir moteris skirtinga fono spalva (className)
---
+++ src/components/persons/PersonList.jsx
@@ -0,0 +1,7 @@
+// importuosim is data.js duomenis
+
+// suksim cikla ir generuosim Person componentus
+
+// (sunkesnis veliau)atrinkti tik vyriskos lyties persons
+
+// exportuoti ir importuot i App.jsx
