85e4e317e18e6e8b27ad34d24dc3719b911def7b
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-09 08:34:03 +0300

    makeSpecial

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,22 @@
 import { useState } from 'react';
 
 function Card(props) {
-  const [isItSpecial, setIsItSpecial] = useState();
-  // console.log('props ===', props);
-  // const heading = props.heading;
-  // const text = props.text;
+  const [isItSpecial, setIsItSpecial] = useState(false);
+
+  function makeSpecial() {
+    console.log('makeSpecial');
+    setIsItSpecial(true);
+  }
+
+  const computeClass = isItSpecial ? 'special card' : 'card';
+  console.log('computeClass ===', computeClass);
+
   const { heading, text } = props;
   return (
-    <div className='card'>
+    <div className={computeClass}>
       <h3 className='cardTitle'>{heading}</h3>
       <p className='cardText'>{text}</p>
-      <button>Make special</button>
+      <button onClick={makeSpecial}>Make special</button>
     </div>
   );
 }
