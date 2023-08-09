26bf662b64aadfa82e45c70a4136f40eaaa19a23
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-09 08:43:02 +0300

    toggleSpecial normal

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,6 +8,12 @@ function Card(props) {
     setIsItSpecial(true);
   }
 
+  function toggleSpecial() {
+    // turim padaryti true jei yra false, false jei yra true;
+    // setIsItSpecial(isItSpecial ? false : true);
+    setIsItSpecial(!isItSpecial);
+  }
+
   const computeClass = isItSpecial ? 'special card' : 'card';
   console.log('computeClass ===', computeClass);
 
@@ -16,7 +22,9 @@ function Card(props) {
     <div className={computeClass}>
       <h3 className='cardTitle'>{heading}</h3>
       <p className='cardText'>{text}</p>
-      <button onClick={makeSpecial}>Make special</button>
+      <button onClick={toggleSpecial}>
+        Make {isItSpecial ? 'normal' : 'special'}
+      </button>
     </div>
   );
 }
